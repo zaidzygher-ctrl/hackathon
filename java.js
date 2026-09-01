ec6c5f2568263bcc276e2b449eac441c8d422160
ZAID AL-ZYGHYER <zaidzygher@gmail.com>
2026-09-01 13:04:28 +0300

    v7

--- java.js
+++ java.js
@@ -1,56 +1,56 @@
 const img1=document.querySelector("#JC1");
 img1.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="garcia.html";
 });
 
 const img2=document.querySelector("#JC2");
 img2.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="cancelo.html";
 });
 
 const img3=document.querySelector("#JC3");
 img3.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="martin.html";
 });
 
 const img4=document.querySelector("#JC4");
 img4.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="cubarsi.html";
 });
 const img5=document.querySelector("#JC5");
 img5.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="kounde.html";
 });
 const img6=document.querySelector("#JC6");
 img6.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="pedri.html";
 });
 const img7=document.querySelector("#JC7");
 img7.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="dejong.html";
 });
 const img8=document.querySelector("#JC8");
 img8.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="olmo.html";
 });
 const img9=document.querySelector("#JC9");
 img9.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="rodri.html";
 });
 const img10=document.querySelector("#JC10");
 img10.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="yamal.html";
 });
 const img11=document.querySelector("#JC11");
 img11.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="raphinha.html";
 });
 const img12=document.querySelector("#JC12");
 img12.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="gordon.html";
 });
 const img13=document.querySelector("#JC13");
 img13.addEventListener("click",()=>{
-    window.location.href="https://google.com";
+    window.location.href="adeyemi.html";
 });
 
