1fd0ead0a47e598dfcf91188fb7569bd1090ba26
ZAID AL-ZYGHYER <zaidzygher@gmail.com>
2026-09-01 12:28:52 +0300

    v3

---
+++ java.js
@@ -0,0 +1,56 @@
+const img1=document.querySelector("#JC1");
+img1.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+
+const img2=document.querySelector("#JC2");
+img2.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+
+const img3=document.querySelector("#JC3");
+img3.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+
+const img4=document.querySelector("#JC4");
+img4.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img5=document.querySelector("#JC5");
+img5.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img6=document.querySelector("#JC6");
+img6.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img7=document.querySelector("#JC7");
+img7.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img8=document.querySelector("#JC8");
+img8.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img9=document.querySelector("#JC9");
+img9.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img10=document.querySelector("#JC10");
+img10.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img11=document.querySelector("#JC11");
+img11.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img12=document.querySelector("#JC12");
+img12.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+const img13=document.querySelector("#JC13");
+img13.addEventListener("click",()=>{
+    window.location.href="https://google.com";
+});
+
